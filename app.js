16130e09ef1091eedaae45b1bc12f7f324f4154f
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-12-01 13:24:07 +0100

    added canvas with a littlebit of js (getContext creating big object with big amount of methods)

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ let buttonBackground = document.querySelector('#button-background');
 button.addEventListener('click', () => {
     console.log(buttonBackground.classList);
     buttonBackground.classList.toggle('background');
-})
+});
+
+let canvas = document.querySelector('canvas');
+
+console.dir(canvas);
+canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+// console.dir(window);
+// console.dir(canvas);
+// console.log(document.body.style);
+let ctx = canvas.getContext('2d');
+ctx.fillRect(10, 10, 100, 100);
+ctx.fillRect(120, 10, 100, 100);
+ctx.fillRect(230, 20, 100, 100);
+
 
 
